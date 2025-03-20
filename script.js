46d6205a516044b79518c09329bebd6d30456303
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-03-20 08:55:59 -0300

    add função calcular

--- script.js
+++ script.js
@@ -9,5 +9,15 @@ if (Numero % 2 === 0) {
     console.log( Numero + "é um número é ímpar");
 }
 }
+
+function calcular(n1,n2) {
+       console.log(n1 + n2)
+       console.log(n1 * n2)
+       console.log(n1 - n2)
+       console.log(n1 / n2);
+}
+
 //chamando função
-verificarPar();
+//verificarPar();
+calcular()
+
