2bc318a10af2beeccd07803ff199d46edc2d2656
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-03-20 08:31:16 -0300

    add estruturas de controle

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 //Variáveis
-
+let Numero = 10
 //Funções
 function verificarPar() {
-    console.log("")
+    console.log(Numero)
 }
 //chamando função
+verificarPar()
+//Estruturas de controle
+if (Numero % 2 === 0) {
+    console.log("Esse número é par")
+} else {
+    console.log("Esse número é ímpar")
+}
