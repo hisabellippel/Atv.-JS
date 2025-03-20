e5e1cc6da185dd6353453c6ce2b0da94c900ff97
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-03-20 09:21:21 -0300

    add tirar =

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 //Variáveis
 //let Numero = prompt('Digite um número');
-let n1 = parseFloat (prompt = ("digite um número"));
-let n2 = parseFloat (prompt = ("digite outro número"));
+let n1 = parseFloat(prompt('digite um número'));
+let n2 = parseFloat(prompt('digite outro número'));
 
 //Funções
 function verificarPar() {
 
-if (Numero % 2 === 0) {
-    console.log( Numero + "é um número par");
-} else {
-    console.log( Numero + "é um número é ímpar");
-}
+    if (Numero % 2 === 0) {
+        console.log(Numero + "é um número par");
+    } else {
+        console.log(Numero + "é um número é ímpar");
+    }
 }
 
-function calcular(n1,n2) {
-       console.log(n1 + n2);
-       console.log(n1 * n2);
-       console.log(n1 - n2);
-       if ( n1 === 0) {
-        console.log("não é possível resolver");
-       } else {
+function calcular(n1, n2) {
+    console.log(n1 + n2);
+    console.log(n1 * n2);
+    console.log(n1 - n2);
+    if (n1 === 0) {
+        console.log("Não é possível resolver");
+    } else {
         console.log(n1 / n2);
-       }
+    }
 }
 
 //chamando função
 //verificarPar();
-calcular(n1,n2)
+calcular(n1, n2);
 
